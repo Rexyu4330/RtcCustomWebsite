e75e3bc9514ca58e662fe395e15a55ef227e3b33
Rexyu4330 <mathias.boudreau@gmail.com>
2019-11-04 17:39:03 -0500

    Add mobile branch

--- website/js/showRoute.js
+++ website/js/showRoute.js
@@ -20,9 +20,15 @@ function makeStopsMarkers(bus, dir, codeType, listeArret) {
         m.bus = bus;
         m.dir = dir;
         m.codeType = codeType;
+	
         
         m.addTo(mymap);
         listMarkersArrets.push(m);
+	//Testing zone on mobile
+	console.log('test');
+	/*m.on('popupopen', function(ev) {
+            console.log('IT WORKS');
+	}*/
     }
 }
 
@@ -31,4 +37,8 @@ function hideRoute() {
         listMarkersArrets[y].remove();
     }
     listMarkersArrets = [];
+}
+/*
+mymap.on('popupopen', function(e) {
+    console.log(e);
 }
